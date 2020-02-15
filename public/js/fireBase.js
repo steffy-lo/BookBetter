f1ec3379d1a47fed7e67806bf077d865f2199bed
VicLo <victoria2666@gmail.com>
2020-02-14 23:29:25 -0500

    Connect to firebase

--- public/js/fireBase.js
+++ public/js/fireBase.js
@@ -1,18 +1,17 @@
 var app_fireBase = {};
 (function(){
-    // Your web app's Firebase configuration
     var firebaseConfig = {
-        apiKey: "AIzaSyByKShoow_gskdrquQlGjwRTl5NzlrKCtM",
-        authDomain: "caht-viclo.firebaseapp.com",
-        databaseURL: "https://caht-viclo.firebaseio.com",
-        projectId: "caht-viclo",
-        storageBucket: "caht-viclo.appspot.com",
-        messagingSenderId: "285312197444",
-        appId: "1:285312197444:web:e109279f0a5d6fcc3fed11",
-        measurementId: "G-RXC4GHM2R9"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+        apiKey: "AIzaSyAx3dpt2IHMOPeukRN6T4cfuaq44oYKlpI",
+        authDomain: "bookbetter-ec657.firebaseapp.com",
+        databaseURL: "https://bookbetter-ec657.firebaseio.com",
+        projectId: "bookbetter-ec657",
+        storageBucket: "bookbetter-ec657.appspot.com",
+        messagingSenderId: "551567741715",
+        appId: "1:551567741715:web:daae83382b85d26da400e6",
+        measurementId: "G-9E494YXLGD"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
     app_fireBase = firebase;
 
 })()
