1e2eb4d59f5bf51b393f41f1f9e82a64953c98ad
VicLo <victoria2666@gmail.com>
2020-02-15 03:14:54 -0500

    commit calendar

--- public/js/appointments.js
+++ public/js/appointments.js
@@ -2,7 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
-      plugins: [ 'dayGrid' ]
+      plugins: [ 'dayGrid', 'timeGrid'],
+      defaultView: 'timeGridWeek'
     });
 
     calendar.render();
