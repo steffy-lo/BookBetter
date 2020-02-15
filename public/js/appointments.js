5b5c30706001ba9b9ec3c7ca226bb9366671cf18
VicLo <victoria2666@gmail.com>
2020-02-15 11:09:06 -0500

    commit calendar setup

--- public/js/appointments.js
+++ public/js/appointments.js
@@ -1,10 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('calendar');
+  var calendarEl = document.getElementById('calendar');
 
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      plugins: [ 'dayGrid', 'timeGrid', 'interaction'],
-      defaultView: 'timeGridWeek'
-    });
-
-    calendar.render();
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    plugins: [ 'interaction', 'dayGrid', 'timeGrid' ],
+    selectable: true,
+    defaultView: 'timeGridWeek',
+    nowIndicator: true,
+    minTime: '8:00',
+    maxTime: '18:00',
+    select: function(info) {
+      info.startStr = calendar.formatDate(info.start, {hour:'numeric',minute:'2-digit'});
+      info.endStr = calendar.formatDate(info.end, {hour:'numeric',minute:'2-digit'});
+      alert('Book Appointment from '+ info.startStr+' to '+info.endStr+'?');
+      calendar.addEvent({
+        title: 'Appointment',
+        start: info.start,
+        allDay: false
+      });
+    },  
   });
+
+  calendar.render();
+});
